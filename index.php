0b49e5b37cf3edf4a3068bd54c05b8698a609ff1
Douglas Almeida dos Santos <douglas@rudinei.cnt.br>
2014-04-23 10:27:06 -0300

    Teste
    teste

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <meta name="autor" content="Douglas Santos" />
+        <meta name="keywords" content="Crie Design, design, webdesign, portifolio, serviços, css, html, html5" />
+        <meta name="description" content="CrieDesign - Simplicidade e o último grau de sofisticação" />
+        <link rel="stylesheet" href="estilo.css" />
+        <title>Crie Design</title>
+    </head>
+    <body>
+        <div id="container">
+            <header>
+                <a href="#" id="logo"><img src="img/logo1.png" title="logo"></a>
+            </header>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Sobre</a></li>
+                    <li><a href="#">Portifólio</a></li>
+                    <li><a href="#">Serviços</a></li>
+                    <li><a href="#">Contato</a></li>
+                </ul>
+            </nav>
+            <section id="intro">
+                <hgroup>
+                    <h1>
+                        "Simplicidade e o último grau de sofisticação"<span class="autor"> - Leonardo da Vinci</span>
+                    </h1>
+                    <h2 class="linha">
+                        &nbsp;&nbsp;Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vitae tellus libero. In id odio ante, nec convallis magna. Morbi quis eros sit amet sapien ultrices auctor sit amet a nibh. Sed sed justo vitae libero congue tempus sed eget nisl. Morbi consectetur luctus dui eu convallis. Morbi commodo, lorem nec vestibulum tincidunt, mi nulla pretium nisl, non pellentesque mauris lacus ut nisl.
+                    </h2>
+                </hgroup>
+            </section>
+            <div id="content">
+                <section class="sobre">
+                    <h3>Sobre</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vitae tellus libero. In id odio ante, nec convallis magna. Morbi quis eros sit amet sapien ultrices auctor sit amet a nibh. Sed sed justo vitae libero congue tempus sed eget nisl. Morbi consectetur luctus dui eu convallis. Morbi commodo, lorem nec vestibulum tincidunt, mi nulla pretium nisl, non pellentesque mauris lacus ut nisl.
+                    </p>
+                    <a href="#"><img src="img/img1.jpg"></a>
+                    <button>Leia Mais</button>
+                </section>
+                <section class="servicos">
+                    <h3>Serviços</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vitae tellus libero. In id odio ante, nec convallis magna. Morbi quis eros sit amet sapien ultrices auctor sit amet a nibh. Sed sed justo vitae libero congue tempus sed eget nisl. Morbi consectetur luctus dui eu convallis. Morbi commodo, lorem nec vestibulum tincidunt, mi nulla pretium nisl, non pellentesque mauris lacus ut nisl.
+                    </p>
+                    <a href="#"><img src="img/img2.jpg"></a>
+                    <button>Leia Mais</button>
+                </section>
+                <section class="noticias">
+                    <h3>Notícias</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vitae tellus libero. In id odio ante, nec convallis magna. Morbi quis eros sit amet sapien ultrices auctor sit amet a nibh. Sed sed justo vitae libero congue tempus sed eget nisl. Morbi consectetur luctus dui eu convallis. Morbi commodo, lorem nec vestibulum tincidunt, mi nulla pretium nisl, non pellentesque mauris lacus ut nisl.
+                    </p>
+                    <a href="#"><img src="img/img3.jpg"></a>
+                    <button>Leia Mais</button>
+                </section>
+                <br style="clear: both">
+            </div>
+            <div id="content">
+                <div class="artigo">
+                    <article>
+                        <h3>22.08.2011 - Design</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vitae tellus libero. In id odio ante, nec convallis magna. Morbi quis eros sit amet sapien ultrices auctor sit amet a nibh. Sed sed justo vitae libero congue tempus sed eget nisl. Morbi consectetur luctus dui eu convallis. Morbi commodo, lorem nec vestibulum tincidunt, mi nulla pretium nisl, non pellentesque mauris lacus ut nisl.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vitae tellus libero. In id odio ante, nec convallis magna. Morbi quis eros sit amet sapien ultrices auctor sit amet a nibh. Sed sed justo vitae libero congue tempus sed eget nisl. Morbi consectetur luctus dui eu convallis. Morbi commodo, lorem nec vestibulum tincidunt, mi nulla pretium nisl, non pellentesque mauris lacus ut nisl.</p>
+                    </article>
+                </div>
+            </div>
+        </div><!-- Fim Container -->
+        <footer>
+            <div id="container">
+                <div class="footer1">
+                    
+                </div>
+                <div class="footer2">
+                    
+                </div>
+            </div>
+        </footer>
+    </body>
+</html>
